d8cb4ac71ad6b48564e82f419d151258397aaebe
Romel16 <romel_faustino@hotmail.com>
2024-02-26 23:56:56 -0500

    Agregando usuario al controllers

---
+++ controllers/usuarioControllers.php
@@ -0,0 +1,17 @@
+<?php
+    /* TODO: Utiliza los archivos de configuracion de la conexion y de la clase usuarioModels */
+    require_once("../config/conexion.php");
+    require_once("../models/usuarioModels.php");
+    /* TODO: Crea una instancia de la clase usuarioModels */
+    $usuario = new usuarioModels();
+
+    /* TODO: Utiliza la estructura switch para determinar la operacion a realizar segun el valor de $_GET["op"] */
+    switch ($_GET["op"]) {
+        /* TODO: Si la operacion es registrar */
+        case "register":
+            /* TODO: Llama al metodo register_user de la instancia $usuario  con los datos del formulario */
+            $usuario->register_user($_POST["usuarioNombreApellido"], $_POST["usuarioCorreo"], $_POST["usuarioPassword"]);
+            break;
+        
+       
+    }
